42579ef07db45644ed8eac6c2bebed6968fd3e13
andrewkivela <andrewkivela@mail.usf.edu>
2017-02-23 18:57:19 -0500

    Added reader for NFC keys and input from keypad. THere is dummy info for checking the keys not quite implemented yet though.

--- ProjectMain/ProjectMain.ino
+++ ProjectMain/ProjectMain.ino
@@ -1,7 +1,13 @@
-#include <SoftwareSerial.h>
-#include <ESP8266WiFi.h>
-
-SoftwareSerial serialOut(14, 12, false, 256);
+//#include <Wire.h>
+//#include <SoftwareSerial.h>
+//#include <ESP8266WiFi.h>
+#include <Wiegand.h>
+//#define wifiEN 0;
+WIEGAND wg; //catches data from keypad
+//SoftwareSerial Serial(14, 12, false, 256); //save for possible future use 
+const char* keyA = "123WRX";
+const char* keyB = "113WEX";
+const char* keyC = "223WRT";
 
 const char* ssid     = "Q46JP_EXT";
 const char* password = "92DDA91C4D";
@@ -9,85 +15,38 @@ const char* password = "92DDA91C4D";
 const char* host = "data.sparkfun.com";
 const char* streamId   = "....................";
 const char* privateKey = "....................";
+char* keyString;
 
 void setup() {
-  // put your setup code here, to run once:
-  // includes for PN532, internet, display
-  //wemos wifi library headers
-  Serial.begin(115200);
-  serialOut.begin(115200);
+//get serial going
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  Serial.println("Arduino started");
 
-  serialOut.println("\n\nArduino started\n");
-  /* //test serial output
-  for (char ch = ' '; ch <= 'z'; ch++) {
-    serialOut.write(ch);
-  }
-  serialOut.println("\n");
-*/
   delay(10);
 
   // We start by connecting to a WiFi network
 
-  serialOut.println();
-  serialOut.print("Connecting to ");
-  serialOut.println(ssid);
-  
+//connect to internet & Azure
+//wifi setup
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+#if defined (wifiEN)
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    serialOut.print(".");
+    Serial.print(".");
   }
 
-  serialOut.println("");
-  serialOut.println("WiFi connected");  
-  serialOut.println("IP address: ");
-  serialOut.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  /*
-
-//NFC lib headers & definitions
-#include <PN532.h>
-#include <SPI.h>
-#define PN532_CS 10
-PN532 nfc(PN532_CS);
-#define  NFC_DEMO_DEBUG 1
-//lcd library headers
-#define bool ok = FALSE
-*/
-
-//check for NFC shield, keypad
-//NFC setup
-/*
-#ifdef NFC_DEMO_DEBUG
-  Serial.begin(9600);
-  Serial.println("Hello!");
 #endif
-  nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-#ifdef NFC_DEMO_DEBUG
-    Serial.print("Didn't find PN53x board");
-#endif
-    while (1); // halt
-  }
-#ifdef NFC_DEMO_DEBUG
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); 
-  Serial.println((versiondata>>24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); 
-  Serial.print((versiondata>>16) & 0xFF, DEC);
-  Serial.print('.'); 
-  Serial.println((versiondata>>8) & 0xFF, DEC);
-  Serial.print("Supports "); 
-  Serial.println(versiondata & 0xFF, HEX);
-#endif
-  // configure board to read RFID tags and cards
-  nfc.SAMConfig();
- */
-//connect to internet & Azure
-//wifi setup
+  
 
 //initialize display 
 
@@ -100,48 +59,75 @@ int value = 0;
 void loop() {
   // put your main code here, to run repeatedly:
 //listen for key from NFC
+
 //if key, then start keypad
 //listen for keypad
 //if keypad check key & pin against azure
 //if match then open, else log & block where applicable
 //open
-  while (serialOut.available() > 0) {
-    Serial.write(serialOut.read());
+  while (Serial.available() > 0) {
+    Serial.write(Serial.read());
   }
   while (Serial.available() > 0) {
-    serialOut.write(Serial.read());
+    Serial.write(Serial.read());
   }
   delay(5000);
   ++value;
-
-  serialOut.print("connecting to ");
-  serialOut.println(host);
+#if defined (wifiEN)
+  Serial.print("connecting to ");
+  Serial.println(host);
   
   // Use WiFiClient class to create TCP connections
+
   WiFiClient client;
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
-    serialOut.println("connection failed");
+    Serial.println("connection failed");
     return;
   }
   
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
-      serialOut.println(">>> Client Timeout !");
+      Serial.println(">>> Client Timeout !");
       client.stop();
       return;
-        serialOut.println();
-  serialOut.println("closing connection");
+        Serial.println();
+  Serial.println("closing connection");
+  
     }
   }
-  /* receive data from server
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    serialOut.print(line);
+  #endif
+  //check for NFC shield, keypad
+  wg.begin();
+
+  if(wg.available())
+  {
+    Serial.print("Wiegand HEX = ");
+    Serial.print(wg.getCode(),HEX);
+    Serial.print(", DECIMAL = ");
+    //strcpy(keyString, wg.getCode());
+    //Serial.print(keyString);
+    Serial.print(wg.getCode());
+    Serial.print(", Type W");
+    Serial.println(wg.getWiegandType());    
+    
+  if(keyString == (keyA || keyB || keyC)) 
+  {
+    
+    while(keyString != 13){
+    //push buttons to string   
+    strcpy(keyString, wg.getCode());
+    }
+  } else if (keyString = "2496273"){
+  Serial.println("Blacklisted card");
+  } else {
+  keyString = "";
   }
-  */
+  Serial.println(keyString);
+  }
+  
+
 
 
   /*
